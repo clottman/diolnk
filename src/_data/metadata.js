3d3a0c5d7fd86497d2365e1a8bcc35f2e684ad9e
Cassey Lottman <clottman@users.noreply.github.com>
2022-07-07 12:58:21 -0500

    social image

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -11,11 +11,5 @@ module.exports = async function () {
     description:
       'Do It Ourselves Fest is a one-day event in Lincoln, Nebraska, celebrating DIY culture, art, music, and mutual aid.',
     social_description: "Do It Ourselves Fest is a one-day event in Lincoln, Nebraska, celebrating DIY culture, art, music, and mutual aid.",
- //    favicon: "/img/favicon.ico",
-  /*  social_image: social_image,
-    social_image_alt: "cassey.dev",
-    social_image_height: 900,
-    social_image_width: 1600, 
-    twitter_card_style: "summary_large_image", */
   };
 };
